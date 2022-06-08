d83eb8e8d8b3db74b8d1640eee026103f1249f7c
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-06-08 16:34:31 -0300

    Receber input do usuário

--- Idade/Program.cs
+++ Idade/Program.cs
@@ -1,9 +1,13 @@
-﻿var nomeDoUsuario = "João";
-var idadeDoUsuario = 20;
+Console.Write("Digite seu nome: ");
+var nomeDoUsuario = Console.ReadLine();
+
+Console.Write("Digite sua idade: ");
+var idadeDoUsuario = Console.ReadLine()!;
+
 var anoAtual = 2022;
 
 // Utilize a mensagem a seguir como base do retorno esperado:
 // Olá João, você nasceu em XXXX.
-var anoDoNascimento = anoAtual - idadeDoUsuario;
+var anoDoNascimento = anoAtual - int.Parse(idadeDoUsuario);
 Console.Write($"Olá {nomeDoUsuario}, você nasceu em {anoDoNascimento}");
 

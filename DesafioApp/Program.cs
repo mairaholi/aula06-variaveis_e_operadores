cca198d32b4fa274653e7ffd583fb24878c0af3c
Marcelo alves <marcelloalves09@gmail.com>
2022-06-08 16:49:19 -0300

    Adicionado o imput

--- DesafioApp/Program.cs
+++ DesafioApp/Program.cs
@@ -1,7 +1,8 @@
-var quilometrosPercorridosPeloVeiculo = 80000;
+Console.Write("Digite quantos quilometros foram percorridos: ");
+var quilometrosPercorridosPeloVeiculo = Console.ReadLine();
 
 // Utilize a mensagem a seguir como base do retorno esperado:
 // Seu veículo percorreu um total de XXXXXX milhas.
 
-var milhasPercorridasPeloVeiculo = quilometrosPercorridosPeloVeiculo * 0.62137;
+var milhasPercorridasPeloVeiculo = int.Parse(quilometrosPercorridosPeloVeiculo) * 0.62137;
 Console.WriteLine($"Seu veículo percorreu um total de {milhasPercorridasPeloVeiculo} milhas.");

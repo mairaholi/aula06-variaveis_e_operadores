62396b3ef493f9b34233b8775d7b420439e0b24c
Marcelo alves <marcelloalves09@gmail.com>
2022-06-08 16:30:25 -0300

    Adicionei o ano do nascimento

--- Idade/Program.cs
+++ Idade/Program.cs
@@ -4,4 +4,6 @@ var anoAtual = 2022;
 
 // Utilize a mensagem a seguir como base do retorno esperado:
 // Olá João, você nasceu em XXXX.
+var anoDoNascimento = anoAtual - idadeDoUsuario;
+Console.Write($"Olá {nomeDoUsuario}, você nasceu em {anoDoNascimento}");
 
